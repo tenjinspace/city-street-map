9b277f72dc1ba5b78326411e75ca66b8d2d8f0a2
tenjinspace <tenjinspace@protonmail.com>
2022-11-07 00:08:54 +0100

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,36 +2,27 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-def main():
-    st.title("City Street Map")
-    st.markdown("with [Geopandas](https://geopandas.org/) and [Streamlit](https://streamlit.io/)")
+st.title("City Street Map")
+st.markdown("with [Geopandas](https://geopandas.org/) and [Streamlit](https://streamlit.io/)")
 
-    st.header("Genoa, IT")
+st.header("Genoa, IT")
 
-    # Import all roads IT
-    map_df = gpd.read_file('roads.shp')
-    # Show data format
-    map_df.head()
+# Import all roads IT
+map_df = gpd.read_file('roads.shp')
+# Show data format
+st.dataframe(map_df.head())
 
-    # Set image properties
-    fig, ax = plt.subplots(1, figsize=(10,14))
-    map_df.plot(cmap='Wistia', ax=ax)
-    ax.axis('off')
+# Set image properties
+fig, ax = plt.subplots(1, figsize=(10,14))
+map_df.plot(cmap='Wistia', ax=ax)
+ax.axis('off')
 
-    genoa = {'lat': 44.4030, 'lon': 8.9449}
+genoa = {'lat': 44.4030, 'lon': 8.9449}
 
-    # Set coordinates to match Genoa
-    ax.set_xlim(genoa['lon']-0.0180, genoa['lon']+0.0180)
-    ax.set_ylim(genoa['lat']-0.0248, genoa['lat']+0.0248)
-    ax.set_aspect('equal')
+# Set coordinates to match Genoa
+ax.set_xlim(genoa['lon']-0.0180, genoa['lon']+0.0180)
+ax.set_ylim(genoa['lat']-0.0248, genoa['lat']+0.0248)
+ax.set_aspect('equal')
 
-    # Plot the street map of Utrecht
-    plt.show()
-
-
-
-
-    
-
-if __name__ == "__main__":
-    main()
+# Plot the street map of Genoa
+st.pyplot(ax)
