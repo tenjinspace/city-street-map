035961310929ed9d3cad0647eb0c23990bf72c01
tenjinspace <tenjinspace@protonmail.com>
2022-11-06 23:45:44 +0100

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
-import geopandas as gpd
-import matplotlib.pyplot as plt
+# import geopandas as gpd
+# import matplotlib.pyplot as plt
+import streamlit as st
 
 def main():
     st.title("City Street Map")
     st.markdown("with [Geopandas](https://geopandas.org/) and [Streamlit](https://streamlit.io/)")
 
     st.header("Genoa, IT")
+
+if __name__ == "__main__":
+    main()
